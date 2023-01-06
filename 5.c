a5a4d653e0abd67057c9f53ab4cd662e12ca6290
ShivanshuPanwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-06 23:53:25 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+   printf("Enter num1");
+   scanf("%d",&num1);
+   printf("Enter num2");
+   scanf("%d",&num2);
+   printf("Sum=%d",sum=num1+num2);
+    return 0;
+}
