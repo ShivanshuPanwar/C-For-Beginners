2163c466d48d2d4bbd5cc02dee956a1fcfd1331c
ShivanshuPanwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-06 23:57:43 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("a");
+    scanf("%d",&a);
+    if(a+5>=10)
+    {
+        printf("mohan");
+    }
+    else
+        printf("bittu");
+        return 0;
+}
