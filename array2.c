3c8338314fc8026a224d1e27a9e1ba444f86d458
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-02-09 18:59:12 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,12 @@
+// get in output of all values which is given in array
+#include<stdio.h>
+int main()
+{
+    int i;
+    int data[3]={5,10,15};
+    for(i=0;i<3;i++)
+    {
+        printf("%d\t",data[i]);
+    }
+    return 0;
+}
