8e2950ea128a240b6301ca0f8c4d5315729a4200
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-02-09 18:58:44 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,12 @@
+//array of one dimension
+
+#include<stdio.h>
+int main()
+{
+    //int data[4] = {1,2,3,4};
+    int data[2];
+    data[0]=1;
+    data[1]=2;
+    printf("%d",data[0]);
+    return 0;
+}
